66600d952c0ee128ee273aacc99d08a13730ea9c
hoomanM77 <107865202+hoomanM77@users.noreply.github.com>
2022-10-06 16:20:24 +0330

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,6 +1,6 @@
 window.addEventListener('load',()=>{
     if(!document.cookie && !authorizeCookie()){
-        location.href='http://localhost:63342/cookie%20project/login.html?_ijt=n2rlr947rn316k4pe9iv0l1g0g&_ij_reload=RELOAD_ON_SAVE'
+        location.href=location.host + '/login.html'
     }
 })
 const authorizeCookie=()=>{
